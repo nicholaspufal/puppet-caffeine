6d443d9cf71e83ff98355320d6080007c927cc42
Nicholas Pufal <npufal@thoughtworks.com>
2013-03-09 10:46:50 -0300

    Added a custom plist.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,17 @@
 #
 #   include caffeine
 class caffeine {
+
+  $user = "/Users/${hostname}"
+
   package { 'Caffeine':
     provider => 'compressed_app',
     source   => 'http://lightheadsw.com/files/releases/com.lightheadsw.Caffeine/Caffeine1.1.1.zip'
+  }
+
+  file { 'caffeine_config':
+    path => "${user}/Library/Preferences/com.lightheadsw.caffeine.plist",
+    content => template('caffeine/com.lightheadsw.caffeine.plist.erb'),
+    require => Package['boxen/caffeine']
   }
 }
