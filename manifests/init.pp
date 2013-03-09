99e4fd71189790e4f65d9ab160c238b409686cc0
Nicholas Pufal <npufal@thoughtworks.com>
2013-03-09 14:11:05 -0300

    Yay! Automatically launches Caffeine after install :D

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,9 +11,15 @@ class caffeine {
     source   => 'http://lightheadsw.com/files/releases/com.lightheadsw.Caffeine/Caffeine1.1.1.zip'
   }
 
-  file { 'caffeine_config':
+  file { 'config caffeine':
     path => "${user_folder}/Library/Preferences/com.lightheadsw.caffeine.plist",
     content => template('caffeine/com.lightheadsw.caffeine.plist.erb'),
     require => Package['Caffeine']
+  }
+
+  exec { 'launch caffeine':
+    command     => '/usr/bin/open /Applications/Caffeine.app',
+    refreshonly => true,
+    subscribe   => File['config caffeine']
   }
 }
